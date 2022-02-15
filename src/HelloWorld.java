026b4a8764a509155644ef42d95336eff80ebcc7
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-15 11:34:37 -0700

    exercises 3 and 4 completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -22,10 +22,27 @@ public class HelloWorld {
         System.out.print("Hello, ");
         System.out.print("World!");
 
-        // syntax ex 1a:
+        // syntax ex 2:
         // create int type variable named myFavoriteNumber; assign favorite number and print
         int myFavoriteNumber = 12;
         System.out.println(myFavoriteNumber);
+
+        // ex 2:
+        // create string type variable named myString; assign a string value and print
+        String myString = "Twelve is my favorite number.";
+        System.out.println(myString);
+
+        // ex 3:
+        // change your code to assign a character value to myString. What do you notice?
+        char myString = 'l';
+        // notice: cannot assign another value to a variable already defined.
+
+        // ex 4:
+        // change your code to assign the value '3.1459' to myString. What happens?
+        double myString = 3.1459;
+        /* notice: still cannot assign another value to a variable already defined.
+         * Numbers (decimals?) also appear differently. "double" is intelliJ's suggested type for this sort of decimal.
+         */
     }
 }
 
