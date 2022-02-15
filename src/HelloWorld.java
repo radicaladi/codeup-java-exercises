79ced1092a74476ea6759433723c6605b6735164
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-15 10:30:59 -0700

    printing hellow world; notes included

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,22 @@
+// class - defines the class
+// HelloWorld is the name of the class and matches the .java file name
+
 public class HelloWorld {
+
+    // method - similar to js function
+    // main method -> specific method in java that acts as our entry point when running java
+    // similar to the starting point of a game.
+
+    public static void main (String[] args){
+        // public – [...]
+        // static – [...]
+        // void – means the method is not expected to return anything
+        // main – name of our method; keyword that is going to be searched as the entry point
+        // String[] args – is passing in a string array that is named args
+
+        // ex 1:
+        // print out "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
 }
+
