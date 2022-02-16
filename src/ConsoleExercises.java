329b4c26fae436a1ecb44b8d367dc33f59971bd3
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-16 10:09:57 -0700

    exercise 1a complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class ConsoleExercises {
+public class ConsoleExercises {
+    public static void main (String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately, %s.", pi);
+    }
 }
