883c278113af5f9e712915d479ce2249733b7cb3
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-16 10:16:09 -0700

    exercise 1b complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main (String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately, %s.", pi);
+        System.out.printf("The value of pi is approximately, %s. %n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        // ex 1b:
+        //Prompt a user to enter an integer and store that value in an int variable using the nextInt method.
+        System.out.print("Enter an integer: ");
+        int userInt = scanner.nextInt();
+
+        System.out.println("You entered: --> \"" + userInt + "\" <--");
+        // What happens if you input something that is not an integer?
+        // ans: throws multiple Scanner errors. Likely because int cannot hold a value that isn't a number
     }
 }
