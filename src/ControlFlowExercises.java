308df8b73c7adb10683a07f75deb5de4b878ecaf
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-17 10:40:49 -0700

    exercise 1a while complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ControlFlowExercises {
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("this is i at: " + i);
+            i++;
+        }
+
+
+
+
+//        NOTES ON LOOPS
+//        for(int i = 0; i <= 10; i++){
+//            System.out.println("i is at: " + i);
+//        }
+
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println("i is: " + i);
+//            i++;
+//        }
+
+//        int i = 0;
+//        do {
+//            System.out.println("i is at: " + i);
+//            i++;
+//        } while (i< 10);
+    }
 }
