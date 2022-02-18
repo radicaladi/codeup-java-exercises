fe5aa8cbf9762a69aa05b7e60d545255a94f41cd
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 09:53:21 -0700

    established bob's dialogue; empty input need configuration

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Bob {
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ask Bob a anything: ");
+        String userInput = scanner.next();
+
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else System.out.println("Whatever.");
+    }
 }
