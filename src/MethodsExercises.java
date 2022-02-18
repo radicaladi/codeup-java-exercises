9bfe707f737d3742f8ed13d42c5fa17ce9074b2b
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 13:44:33 -0700

    second mini exercises added; comment outs made

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println(userName());
+//        System.out.println(userName());
+//        System.out.println(newAddition());
         System.out.println(addition(5, 3));
         System.out.println(subtraction(5, 3));
         System.out.println(multiplication(5, 3));
@@ -12,18 +13,25 @@ public class MethodsExercises {
     }
 
     // class mini ex:
-    public static String userName(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your first name?: ");
-        String user = scanner.next();
-        return "Hello, " + user;
-    }
+//    public static String userName(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your first name?: ");
+//        String user = scanner.next();
+//        return "Hello, " + user;
+//    }
 
     // Basic Arithmetic
 
     public static int addition(int number1, int number2) {
         return number1 + number2;
     }
+//    public static int newAddition(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter two numbers: ");
+//        int num1 = scanner.nextInt();
+//        int num2 = scanner.nextInt();
+//        return num1 + num2;
+//    }
     public static int subtraction(int number1, int number2) {
         return number1 - number2;
     }
