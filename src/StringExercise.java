ef8dfaf5dbf75fb9b305861b716c99eaa1c8d727
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 09:27:12 -0700

    exercise 1a completed

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class StringExercise {
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String sentence = "We don't need no education\n";
+        sentence = sentence + "We don't need no thought control";
+        System.out.println(sentence);
+
+    }
 }
