3da69459bfa3a259bfe8fe50d2a2493796a3bef3
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 09:30:16 -0700

    exercise 1 complete

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,5 +5,15 @@ public class StringExercise {
         sentence = sentence + "We don't need no thought control";
         System.out.println(sentence);
 
+        sentence = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(sentence);
+
+        sentence = "In windows, the main drive is usually C:\\";
+        System.out.println(sentence);
+
+        sentence = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(sentence);
+
     }
 }
