b2846d82118dccf16994fc0ead2cddc900763840
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 11:17:39 -0700

    do while loop added for ease of use; exercise completed

--- src/Bob.java
+++ src/Bob.java
@@ -1,18 +1,29 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ask Bob a anything: ");
-        String userInput = scanner.nextLine();
+        String userInput;
+        String repeat;
+        do {
+            System.out.print("Ask Bob a anything: ");
+            userInput = scanner.nextLine();
 
-        if (userInput.endsWith("?")) {
-            System.out.println("Sure.");
-        } else if (userInput.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        } else if (userInput.equals("") | userInput.equals(" ")) {
-            System.out.println("Fine. Be that way!");
-        } else System.out.println("Whatever.");
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("") | userInput.equals(" ")) {
+                System.out.println("Fine. Be that way!");
+            } else System.out.println("Whatever.");
+            System.out.println("Ask bob another question?: [y/N]");
+            repeat = scanner.nextLine().toLowerCase(Locale.ROOT);
+            if (repeat.equals("n")) {
+                System.out.println("Okay, that's enough for today...");
+            }
+        } while (repeat.equals("y"));
+
     }
 }
