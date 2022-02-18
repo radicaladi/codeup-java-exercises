c416f3be1e4dc274e3cbe1231e27fa3385ea030d
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-18 11:13:15 -0700

    fixed bob dialogue for empty input

--- src/Bob.java
+++ src/Bob.java
@@ -4,14 +4,14 @@ public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ask Bob a anything: ");
-        String userInput = scanner.next();
+        System.out.print("Ask Bob a anything: ");
+        String userInput = scanner.nextLine();
 
         if (userInput.endsWith("?")) {
             System.out.println("Sure.");
         } else if (userInput.endsWith("!")) {
             System.out.println("Whoa, chill out!");
-        } else if (userInput.equals("")) {
+        } else if (userInput.equals("") | userInput.equals(" ")) {
             System.out.println("Fine. Be that way!");
         } else System.out.println("Whatever.");
     }
