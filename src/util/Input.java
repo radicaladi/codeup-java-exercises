6cdea9f49f32367d8ceac9f328cf5f5d564112f1
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-22 15:13:06 -0700

    input validation class 1-4 complete

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,2 +1,37 @@
-package util;public class Input {
+package util;
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input (){
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return scanner.next();
+    }
+    public boolean yesNo (){
+        String input = scanner.next();
+        return input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y");
+    }
+    public int getInt (int min, int max){
+        int input;
+        do {
+            System.out.printf("Enter a number between %s and %s: ", min, max);
+            input = scanner.nextInt();
+        }
+        while ( input < min || input > max);
+        return input;
+    }
+    public int getInt (){
+        return scanner.nextInt();
+    }
+    public double getDouble (double min, double max) {
+
+    }
+    public double getDouble (){
+
+    }
+
 }
