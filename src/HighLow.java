bcd43a15c1026358147df01c78b5e46f6890e7b8
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-22 09:12:55 -0700

    refactor into do...while loop to simulate guess

--- src/HighLow.java
+++ src/HighLow.java
@@ -20,18 +20,24 @@ public class HighLow {
         int numberToGuess = (int) Math.floor(100 * Math.random() + 1);
         System.out.println(numberToGuess); // confirm working at this point
         Scanner castNet = new Scanner(System.in);
-        System.out.println("Guess a number 1-100: ");
-        int userGuess = castNet.nextInt();
-        System.out.println("Your guess is, " + userGuess); // confirm working at this point
-        if (userGuess == numberToGuess) {
-            System.out.println("Woah! You guessed the right number! Awesome!");
+        int userGuess; // put variable outside of loop to make it work
+        do {
+            System.out.println("Guess a number 1-100: ");
+            userGuess = castNet.nextInt();
+            System.out.println("Your guess is, " + userGuess); // confirm working at this point
+
+            if ( userGuess == numberToGuess ) {
+                System.out.println("Woah! You guessed the right number! GOOD GUESS!");
+                break;
+            }
+            else if ( userGuess < numberToGuess ) {
+                System.out.println("hint: HIGHER");
+            }
+            else if ( userGuess > numberToGuess ) {
+                System.out.println("hint: LOWER");
+            }
         }
-        else if ( userGuess < numberToGuess) {
-            System.out.println("HIGHER");
-        }
-        else if ( userGuess > numberToGuess ) {
-            System.out.println("LOWER");
-        }
+        while ( userGuess != numberToGuess);
     }
 
 
