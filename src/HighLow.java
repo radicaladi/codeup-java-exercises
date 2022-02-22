f359f76d5ec1b40ae655242c1a5f39213f7786f9
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-21 17:56:26 -0700

    starting point

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HighLow {
+public class HighLow {
+    // Build a high-low guessing game.
+    // Create a class named HighLow inside of src.
+    //
+    //The specs for the game are:
+    //
+    //Game picks a random number between 1 and 100.
+    //Prompts user to guess the number.
+    //All user inputs are validated.
+    //If user's guess is less than the number, it outputs "HIGHER".
+    //If user's guess is more than the number, it outputs "LOWER".
+    //If a user guesses the number, the game should declare "GOOD GUESS!"
+
+    public static void numberGenerator() {
+        int generatedNumber = (int) Math.floor(100 * Math.random() + 1);
+        System.out.println(generatedNumber);
+    }
 }
