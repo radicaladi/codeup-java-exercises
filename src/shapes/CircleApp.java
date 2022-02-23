a4fb06a655f367576663d8c401feab371b73fc92
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-23 10:54:29 -0700

    enable user input for radius and return calculation of area and circumference

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,12 @@
-package shapes;public class CircleApp {
+package shapes;
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("Enter a radius: ");
+        double submitRadius = input.getDouble();
+        Circle circle = new Circle(submitRadius);
+        System.out.printf("For a circle with radius: %.2f.\nCircumference is: %.2f.\nArea is: %.2f.", submitRadius, circle.getCircumference(), circle.getArea());
+    }
 }
