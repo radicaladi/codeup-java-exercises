e130795d277bd2205b2576316d0fd1c9814598b2
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 15:51:18 -0700

    add rectangle constructor and methods, getPerimeter, getArea

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,4 +3,17 @@ package shapes;
 public class Rectangle {
     protected double length;
     protected double width;
+
+    public double rectangle (double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return length * 2 + width * 2;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
 }
