f3e97d4548042026f7f1950f87cffdad9b575576
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 13:33:32 -0700

    add fields, method, constructor

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,2 +1,23 @@
-package movies;public class Movie {
+package movies;
+
+public class Movie {
+    // fields
+    private String name;
+    private String category;
+
+    // method
+    public String getMovie() {
+        return this.name;
+    }
+
+    // method
+    public String getCategory() {
+        return this.category;
+    }
+
+    // constructor
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 }
