ce71a1887fa03c59764d7902eb65b19faba92fbe
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 12:24:19 -0700

    add adjective and noun arays, test if functional

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ServerNameGenerator {
+import java.util.Arrays;
+
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adjectives = {"invincible", "habitual", "future", "aggressive", "flowery", "sneaky", "magnificent", "ugly", "fine", "ossified"};
+        String[] nouns = {"vehicle", "candidate", "wedding", "mud", "reputation", "collection", "permission", "entry", "affair", "ear"};
+
+        System.out.println(Arrays.toString(adjectives)); // testing output
+        System.out.println(Arrays.toString(nouns)); // testing output
+    }
 }
