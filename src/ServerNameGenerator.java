86f819ab6fbf8b9363c3288e09304a6c36b3a9d3
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 12:33:14 -0700

    randomize choice of array element; output random server name from method

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,18 @@
-import java.util.Arrays;
+import java.util.Random;
 
 public class ServerNameGenerator {
-    public static void main(String[] args) {
-        String[] adjectives = {"invincible", "habitual", "future", "aggressive", "flowery", "sneaky", "magnificent", "ugly", "fine", "ossified"};
-        String[] nouns = {"vehicle", "candidate", "wedding", "mud", "reputation", "collection", "permission", "entry", "affair", "ear"};
+   static String[] adjectives = {"invincible", "habitual", "future", "aggressive", "flowery", "sneaky", "magnificent", "ugly", "fine", "ossified"};
+   static String[] nouns = {"vehicle", "candidate", "wedding", "mud", "reputation", "collection", "permission", "entry", "affair", "ear"};
 
-        System.out.println(Arrays.toString(adjectives)); // testing output
-        System.out.println(Arrays.toString(nouns)); // testing output
+    public static String randomPicker(String[] array) {
+        int picker = new Random().nextInt(array.length);
+        return array[picker];
     }
+
+    public static void main(String[] args) {
+        String pieceAdj = randomPicker(adjectives);
+        String pieceNoun = randomPicker(nouns);
+        System.out.printf("For a server name, try:%n%s-%s.%n", pieceNoun, pieceAdj);
+    }
+
 }
