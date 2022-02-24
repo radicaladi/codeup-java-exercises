92226b5223cb023cee9816302f8e112b59a03033
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 11:09:22 -0700

    refactor array of person objects; test for storage and output; refactor loop

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,14 +5,17 @@ public class ArraysExercises {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
-        Person Amy = new Person("Amy");
-        Person Barry = new Person("Barry");
-        Person Cory = new Person("Cory");
+        Person[] people = new Person[3]; // an array containing 3 objects -- Person[] people is array; new Person[3] is objects
 
-        String[] persons = {Amy.getName(), Barry.getName(), Cory.getName()};
-//        System.out.println(Arrays.toString(persons)); // testing
-        for (int i = 0; i < persons.length; i += 1) {
-            System.out.println(persons[i]);
+//        System.out.println(Arrays.toString(people)); // test if array populates anything
+        people[0] = new Person("Amy");
+        people[1] = new Person("Barry");
+        people[2] = new Person("Cory");
+//        System.out.println(people[0].getName()); // test if array stored Person object -- returning name of Person object
+
+        for (Person person : people) {
+            System.out.println(person.getName());
         }
+
     }
 }
