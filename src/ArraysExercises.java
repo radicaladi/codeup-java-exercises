060dcb09f6b348a4beb03c0102ba823f63031279
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 11:26:49 -0700

    add static method to add person object to people array of person objects

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,9 +1,22 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+
+    public static Person[] addPerson(Person[] people, Person newPerson) {
+        // static method accepting array of objects and person object
+        Person[] newArray = Arrays.copyOf(people, people.length+1);
+        // returning array +1 greater
+        newArray[newArray.length - 1] = newPerson;
+        // with newPerson inserted at end;
+        for (Person person : newArray) {
+            System.out.println(person.getName());
+        }
+        return newArray;
+    }
+
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
 
         Person[] people = new Person[3]; // an array containing 3 objects -- Person[] people is array; new Person[3] is objects
 
@@ -13,9 +26,16 @@ public class ArraysExercises {
         people[2] = new Person("Cory");
 //        System.out.println(people[0].getName()); // test if array stored Person object -- returning name of Person object
 
-        for (Person person : people) {
+        for (int i = 0; i < people.length; i++) {
+            Person person = people[i];
             System.out.println(person.getName());
         }
+        System.out.println(); // space in console
+
+        // create 4th person, people array index 3
+        Person dex = new Person("Dex");
+        // add person to people array
+        addPerson(people, dex);
 
     }
 }
