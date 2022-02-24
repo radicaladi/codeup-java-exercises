1e9565a3963986f0a42af3d6f4a840fb0032ef36
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-24 08:18:54 -0700

    add objects, work in progress

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ArraysExercises {
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public void Person (String name) {
+        System.out.println("Name is: " + name);
+    }
+    public static void main(String[] args) {
+        Person a = new Person("Anton");
+        Person b = new Person("Barry");
+        Person c = new Person("Cory");
+
+        String[] persons = { a, b, c};
+
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
+    }
 }
