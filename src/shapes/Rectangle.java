8d90f733ff6eb8181309f8a34428c5ad625b8626
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-26 18:10:05 -0700

    fix constructor type

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,7 +4,7 @@ public class Rectangle {
     protected double length;
     protected double width;
 
-    public void rectangle (double length, double width) {
+    public Rectangle (double length, double width) {
         this.length = length;
         this.width = width;
     }
