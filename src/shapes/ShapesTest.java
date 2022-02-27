69c0a864fa224cd1d3dd418ff556994dff02ed59
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-26 18:12:51 -0700

    adds functionality test of shape object calculation

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,4 +2,15 @@ package shapes;
 
 public class ShapesTest {
 
+    public static void main(String[] args) {
+        Rectangle neu = new Rectangle(12, 6);
+        System.out.println("neu.getArea() = " + neu.getArea());
+        System.out.println("neu.getPerimeter() = " + neu.getPerimeter());
+
+        Rectangle spaß = new Square(10);
+        System.out.println("spaß.getArea() = " + spaß.getArea());
+        System.out.println("spaß.getPerimeter() = " + spaß.getPerimeter());
+
+    }
+
 }
