1dab338b57566b3b34494912df0b9ef3ab5f41be
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-03 09:35:59 -0700

    adds methods and logic for student grade average

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -11,4 +11,29 @@ public class Student {
         this.studentName = name;
         this.studentGrades = new ArrayList<>();
     }
+
+    // returns the student's name
+    public String getName() {
+        return studentName;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        studentGrades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        int total = 0;
+        int avg;
+        for (Integer studentGrade : studentGrades) total = total + studentGrade;
+        avg = total / studentGrades.size();
+        System.out.println("Student Grade Average: " + avg);
+        return avg;
+// traditional for-loop:
+//        for(int i = 0; i<studentGrades.size(); i++)
+//            total = total+studentGrades.get(i);
+//        avg = total / studentGrades.size();
+//        System.out.println("The Average IS:" + avg);
+    }
 }
