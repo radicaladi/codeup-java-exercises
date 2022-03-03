80606e82576fc07a8e095cf683ff07f8f74e7b53
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-03 13:18:57 -0700

    add getter studentGrades

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,40 +1,35 @@
 package grades;
 
-// ArrayList requires import java util
 import java.util.ArrayList;
 
 public class Student {
     private final String studentName;
     private final ArrayList<Integer> studentGrades;
 
-    // constructor
     public Student (String name){
         this.studentName = name;
         this.studentGrades = new ArrayList<>();
     }
 
-    // returns the student's name
     public String getName() {
         return studentName;
     }
 
-    // adds the given grade to the grades property
     public void addGrade(int grade) {
         studentGrades.add(grade);
     }
 
-    // returns the average of the students grades
+    // returns the student's grade
+    public ArrayList<Integer> getStudentGrades() {
+        return studentGrades;
+    }
+
     public double getGradeAverage() {
         double total = 0;
         double avg;
         for (int studentGrade : studentGrades) total = total + studentGrade;
         avg = total / studentGrades.size();
         return avg;
-// traditional for-loop:
-//        for(int i = 0; i<studentGrades.size(); i++)
-//            total = total+studentGrades.get(i);
-//        avg = total / studentGrades.size();
-//        System.out.println("The Average IS:" + avg);
     }
 
     public static void main(String[] args){
@@ -44,7 +39,7 @@ public class Student {
         Markus.addGrade(78);
         Markus.addGrade(95);
         Markus.addGrade(89);
-        System.out.println(Markus.studentGrades); // [80,78,95,89]
+        System.out.println("Student Grades: " + Markus.getStudentGrades()); //[80,78,95,89]
         System.out.println("Student Grade Average: " + Markus.getGradeAverage()); // 85.5
     }
 }
