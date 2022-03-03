0c45ed8c4ec7e34b5b643124ef64af21e2bf527a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-03 10:08:48 -0700

    initial commit

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,11 @@
+package grades;
+
+import java.util.HashMap;
+
+
+public class GradesApplication {
+    public static void main(String[] args){
+        // HashMap needs import java util
+        HashMap<String, String> students = new HashMap<>();
+    }
+}
