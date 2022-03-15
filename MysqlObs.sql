179dfd14afb0ffd9e38ebb5f5acfe3e6b2ffb0c2
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-15 08:39:18 -0700

    add notes for exercise commands

--- MysqlObs.sql
+++ MysqlObs.sql
@@ -4,3 +4,18 @@
     -- mysql.server restart // restarts mysql
     -- mysql.server stop // discontinues running of mysql
     -- mysql -u root -p // how you log into mysql BUT must make sure mysql is started first
+
+-- SELECT * FROM mysql.user;
+    -- outputs a large list of data that is unreadable
+-- SELECT * FROM mysql.user \G
+    -- outputs the same large list of data in a much easier to read format
+    -- data seems to be 4 rows of permissions statuses
+
+-- SELECT user, host FROM mysql.user;
+    -- outputs a list of users and their sql hosts
+
+-- SELECT * FROM mysql.help_topic
+    -- outputs an incredible amount of help(?) data; 701 rows
+    -- formatted makes it easier to read but still incredibly long
+
+--
